7ee72a01aaf6f90816b17dbd808cbb1afd8d1cd0
dkesada <dkesada@gmail.com>
2024-04-15 16:42:54 +0200

    compacting log extractor

--- api/log_extractor.py
+++ api/log_extractor.py
@@ -1,4 +1,24 @@
 import logging
+import re
+import os
+import sys
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from scipy.spatial.distance import cdist
+
+from autoqchem.descriptor_functions import occupied_volume
+from autoqchem.gaussian_log_extractor import gaussian_log_extractor, NegativeFrequencyException, NoGeometryException, OptimizationIncompleteException
+
+try:
+    from openbabel import pybel  # openbabel 3.0.0
+
+    GetVdwRad = pybel.ob.GetVdwRad
+except ImportError:
+    import pybel  # openbabel 2.4
+
+    table = pybel.ob.OBElementTable()
+    GetVdwRad = table.GetVdwRad
 
 
 logger = logging.getLogger(__name__)
@@ -10,3 +30,5 @@ class LogExtractor:
             self.logger = log
         else:
             self.logger = logger
+
+        self.float_or_int_regex = "[-+]?[0-9]*\.[0-9]+|[0-9]+"
